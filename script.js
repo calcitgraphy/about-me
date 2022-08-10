e8742e263ff1dbf940127245233e0d8800a344ba
Calcit Graphy <88611936+calcitgraphy@users.noreply.github.com>
2022-08-10 10:08:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const sr = ScrollReveal({
+    distance: '90px',
+    duration: 3000,
+})
+
+sr.reveal(`.hedder`, {origin: 'top', delay: 200})
+sr.reveal(`.button`, {origin: 'top', delay: 300})
+sr.reveal(`.dev_img`, {origin: 'bottom', delay: 550})
+sr.reveal(`.social`, {origin: 'top', delay: 400})
+
